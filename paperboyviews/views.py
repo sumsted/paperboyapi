bbafcfa53ebbd9860a4e4e3ac0b1cabd386447aa
Scott Umsted <scottumsted@gmail.com>
2014-09-20 15:27:22 -0500

    added topics api call, just a shell, will build out

--- paperboyviews/views.py
+++ paperboyviews/views.py
@@ -3,6 +3,7 @@ from flask import render_template, make_response, request, json
 from paperboyviews import app, pdb
 import logging
 
+
 def _padded_jsonify(callback, *args, **kwargs):
     content = str(callback) + '(' + json.dumps(dict(*args, **kwargs)) + ')'
     return app.response_class(content, mimetype='application/json')
@@ -14,7 +15,7 @@ def _regular_jsonify(o):
 
 
 @app.route('/stories/', methods=['GET', 'POST'])
-def latest_json():
+def stories():
     result = []
     callback = request.args.get('callback', None)
     try:
@@ -27,4 +28,24 @@ def latest_json():
         result = _regular_jsonify(result)
     else:
         result = _padded_jsonify(callback, {'stories': result})
+    return result
+
+
+@app.route('/topics/', methods=['GET', 'POST'])
+def topics():
+    result = []
+    callback = request.args.get('callback', None)
+    try:
+        stories = pdb.get_stories()
+        if stories is not None:
+            result = []
+            for s in stories:
+                s['related_stories'] = [{'source': 'WREG', 'link': '/story/ASDF'}, {'source': 'WMC', 'link': '/story/ASDF'}]
+                result.append(s)
+    except Exception, e:
+        result = []
+    if callback is None:
+        result = _regular_jsonify(result)
+    else:
+        result = _padded_jsonify(callback, {'topics': result})
     return result
